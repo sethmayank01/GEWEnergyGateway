fc8fcd889fcb1639ea167c63be6a63aa3ba38510
mayank <sethmayank01@gmail.com>
2026-08-09 12:38:18 +0530

    Alpha 9.0.3 - Logger.cpp changes

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -11,78 +11,64 @@
 
 #endif
 
-
-void Logger::Info(const std::string& message)
+namespace
 {
+    void WriteLog(
+        const char* level,
+        const std::string& message)
+    {
 #ifdef PLATFORM_WINDOWS
 
-    std::cout
-        << "[INFO] "
-        << message
-        << std::endl;
+        std::cout
+            << "["
+            << level
+            << "] "
+            << message
+            << std::endl;
 
 #else
 
-    Serial.print("[INFO] ");
-    Serial.println(message.c_str());
+        Serial.print("[");
+
+        Serial.print(level);
+
+        Serial.print("] ");
+
+        Serial.println(message.c_str());
 
 #endif
+    }
 }
 
-
-void Logger::Warning(const std::string& message)
+void Logger::Info(
+    const std::string& message)
 {
-#ifdef PLATFORM_WINDOWS
-
-    std::cout
-        << "[WARNING] "
-        << message
-        << std::endl;
-
-#else
-
-    Serial.print("[WARNING] ");
-    Serial.println(message.c_str());
-
-#endif
+    WriteLog(
+        "INFO",
+        message);
 }
 
-
-void Logger::Error(const std::string& message)
+void Logger::Warning(
+    const std::string& message)
 {
-#ifdef PLATFORM_WINDOWS
-
-    std::cout
-        << "[ERROR] "
-        << message
-        << std::endl;
-
-#else
-
-    Serial.print("[ERROR] ");
-    Serial.println(message.c_str());
-
-#endif
+    WriteLog(
+        "WARNING",
+        message);
 }
 
+void Logger::Error(
+    const std::string& message)
+{
+    WriteLog(
+        "ERROR",
+        message);
+}
 
 void Logger::Hex(
     const std::string& prefix,
     const std::vector<uint8_t>& data)
 {
-#ifdef PLATFORM_WINDOWS
-
-    std::cout
-        << prefix
-        << " "
-        << HexDump::ToString(data)
-        << std::endl;
-
-#else
-
-    Serial.print(prefix.c_str());
-    Serial.print(" ");
-    Serial.println(HexDump::ToString(data).c_str());
-
-#endif
+    WriteLog(
+        "HEX",
+        prefix + " " + HexDump::ToString(data));
 }
